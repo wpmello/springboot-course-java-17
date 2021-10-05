97897f64a5bfe39d1cbf2d988badb98f14b7bb93
wpmello <wagnermello903@gmail.com>
2021-10-05 15:41:08 -0300

    Category

--- src/main/java/com/example/course/entities/enums/OrderStatus.java
+++ src/main/java/com/example/course/entities/enums/OrderStatus.java
@@ -2,6 +2,8 @@ package com.example.course.entities.enums;
 
 public enum OrderStatus {
 
+	// os atributos estao numerados para caso haja uma alteracao no futuro (adicionando ou removendo algum atributo)
+	// o banco de dados nao seja quebrado
 	WAITING_PAYMENT(1),
 	PAID(2),
 	SHIPPED(3),
@@ -18,6 +20,7 @@ public enum OrderStatus {
 		return code;
 	}
 	
+	// metodo estatico para retornar um tipo 'OrderStatus' a partir de um codigo (numero inteiro)
 	public static OrderStatus valueOf(int code) {
 		for (OrderStatus value : OrderStatus.values()) {
 			if (value.getCode() == code) {
