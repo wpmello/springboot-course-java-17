7529cfdcba9892def45df01220277de9742dd0bd
wpmello <wagnermello903@gmail.com>
2021-10-06 12:40:12 -0300

    Exception handling - findById

---
+++ src/main/java/com/example/course/resources/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,26 @@
+package com.example.course.resources.exceptions;
+
+import java.time.Instant;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.example.course.services.exceptions.ResourceNotFoundException;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+	// esta excecao e criada para quando for feita uma requisicao por id e esse mesmo id nao existir,  nao seja 
+	// retornado o tipo '500 Internal Server Erro' que e o padrao generico e sim o tipo '404 Not Found' que e o correto
+	@ExceptionHandler(ResourceNotFoundException.class)
+	public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request) {
+		String error = "Resource not found";
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+}
